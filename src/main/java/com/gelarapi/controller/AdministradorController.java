eec59126f37df17b463cdde911be1b364cdf7f30
Filip <filipe.bonfim@ymail.com>
2019-10-08 12:16:14 -0300

    Endpoint de cadastro de administrador

--- src/main/java/com/gelarapi/controller/AdministradorController.java
+++ src/main/java/com/gelarapi/controller/AdministradorController.java
@@ -2,7 +2,12 @@ package com.gelarapi.controller;
 
 import com.gelarapi.model.Administrador;
 import com.gelarapi.model.Escola;
+import com.gelarapi.model.Usuario;
+import com.gelarapi.repositories.AdministradorDAO;
+import com.gelarapi.repositories.UsuarioDAO;
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,12 +18,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/administrador")
 public class AdministradorController {
 
+    @Autowired
+    private UsuarioDAO usuarioDAO;
+    @Autowired
+    private AdministradorDAO admDAO;
+
     @PostMapping("/novoadm")
     public ResponseEntity<JSONObject> addAdministrador(@RequestBody Administrador adm) {
         JSONObject json = new JSONObject();
         boolean ok = true;
         StringBuilder retMsg = new StringBuilder();
+        Usuario user = new Usuario();
+        if(adm.getLogin() != null && adm.getLogin() != "")
+            user = usuarioDAO.findByLogin(adm.getLogin());
 
+        if(user != null) {
+            retMsg.append("login já cadastrado");
+            ok = false;
+        }
+        else{
+            admDAO.save(adm);
+            retMsg.append("Administrador criado com sucesso");
+        }
 
+        json.put("statusMsg", retMsg.toString());
+
+        return new ResponseEntity<>(json, ok == true ? HttpStatus.CREATED : HttpStatus.OK);
     }
 }
